f6449a4e11e7837cf16bc191436ea2247e0fa067
Ruel Paya <ruel.paya@gmail.com>
2024-05-17 19:01:22 +0800

    TransacrionHistory.jsx
    addl

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -13,10 +13,15 @@ export const TransactionHistory = ({ items }) => {
         </thead>
   
         <tbody>
-          {items.map((item, index) => (
+          {items.slice (0, 5).map((item, index) => (
             <tr className={styles.tableRow} key={index}>
               <td className={styles.tableData}>Invoice</td>
               <td className={styles.tableData}>125</td>
+              <td className={styles.tableData}>USD</td>
+            </tr>
+            <tr>
+              <td className={styles.tableData}>Withdrawal</td>
+              <td className={styles.tableData}>85</td>
               <td className={styles.tableData}>USD</td>
             </tr>
           ))}
